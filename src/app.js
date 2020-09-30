12cc8f2ad0f4422fe77bae155cb2694601b773d6
Yasminn <yasminn.vaz@thoughtworks.com>
2020-09-30 10:33:21 -0300

    atualiza o rodapé da página

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ const musicas = require("./routes/musicasRoutes");
 const cors = require("cors");
 app.use(cors());
 
-app.use("/", musicas);
+app.use('/', musicas);
+
+// apresenta a lista de musicas em forma de tabela
+app.get('/', (req, res) => {
+    res.status(200).sendFile('./views/index.html', { root: __dirname })
+})
 
 module.exports = app;

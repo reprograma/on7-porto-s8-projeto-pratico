47e68958f3fd52f4eeb624d079941ed8838ea6b4
Yasminn <yasminn.vaz@thoughtworks.com>
2020-10-01 20:02:13 -0300

    adicionando uma fonte bonitinha

--- src/app.js
+++ src/app.js
@@ -21,4 +21,8 @@ app.get('/', (req, res) => {
     res.status(200).sendFile('./views/index.html', { root: __dirname })
 })
 
+app.get('*', (req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname })
+})
+
 module.exports = app;

fe8ffaa9194e05d58fb80d81da460adeef1b78d6
Yasminn <yasminn.vaz@thoughtworks.com>
2020-10-01 22:20:34 -0300

    adicionando arquivos estaticos (obrigada Eli <3)

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const musicas = require("./routes/musicasRoutes");
 const cors = require("cors");
 app.use(cors());
 
+app.use(express.static('public'));
+
 app.use('/', musicas);
 
 // apresenta a lista de musicas em forma de tabela
